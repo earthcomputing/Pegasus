1bf6842e889db34d96ca629db5a5ee43ba0dcdc5
alanhkarp <alanhkarp@gmail.com>
2021-08-26 15:37:06 -0700

    Created listen_to_cells()
    Previously talk_to_cell() both sent a message and got the reply, but that's not the way the real code will work.  The simulator will talk to individual cells and get messages from them, not necessarily as send/recv.  For example, the simulator may send chaos monkey messages to the cells, and the cells may send trace records to the simulator.  There is now a separate listen_to_cells() function that will listen to all the cells.

--- src/bin/simulator.rs
+++ src/bin/simulator.rs
@@ -1,6 +1,7 @@
 use std::{
     io::{BufRead, BufReader, Write},
     process::{Child, Command, Stdio},
+    vec,
 };
 fn main() {
     println!("Hello from Pegasus");
@@ -9,10 +10,11 @@ fn main() {
     let mut cell1 = create_cell(cell_id1);
     let mut cell2 = create_cell(cell_id2);
     // Needed to keep simulator running to give cell a chance to finish
-    let reply1 = talk_to_cell(&mut cell1, "Hello 1\n");
-    let reply2 = talk_to_cell(&mut cell2, "Hello 2\n");
-    println!("Simulator got '{}' from {}", reply1, cell_id1);
-    println!("Simulator got '{}' from {}", reply2, cell_id2);
+    talk_to_cell(&mut cell1, "Hello 1\n");
+    talk_to_cell(&mut cell2, "Hello 2\n");
+    let msgs = listen_to_cells(&mut vec![&mut cell1, &mut cell2]);
+    println!("Simulator got '{}' from {}", msgs[0], cell_id1);
+    println!("Simulator got '{}' from {}", msgs[1], cell_id2);
 }
 fn create_cell(cell_id: &'static str) -> Child {
     println!("Starting cell {}", cell_id);
@@ -25,19 +27,26 @@ fn create_cell(cell_id: &'static str) -> Child {
     println!("{} has PID {}", cell_id, cell.id());
     cell
 }
-fn talk_to_cell(cell: &mut Child, msg: &str) -> String {
+fn talk_to_cell(cell: &mut Child, msg: &str) {
     let pid = cell.id();
     println!("Simulator talking to cell with PID {}", pid);
     let to_cell = cell.stdin.as_mut().expect("Cannot get cell stdin");
     to_cell.write(msg.as_bytes()).expect("Cannot write to cell");
-    println!("Simulator listening to cell with PID {}", pid);
-    let from_cell = cell.stdout.as_mut().expect("Cannot get cell stdout");
-    let mut reader = BufReader::new(from_cell).lines();
-    let msg = reader
-        .next()
-        .expect("No message from cell")
-        .expect("Cannot read from cell");
-    msg
+}
+fn listen_to_cells(cells: &mut [&mut Child]) -> Vec<String> {
+    cells
+        .iter_mut()
+        .map(|cell| {
+            let pid = cell.id();
+            println!("Simulator listening to cell with PID {}", pid);
+            let from_cell = cell.stdout.as_mut().expect("Cannot get cell stdout");
+            let mut reader = BufReader::new(from_cell).lines();
+            reader
+                .next()
+                .expect("No message from cell")
+                .expect("Cannot read from cell")
+        })
+        .collect()
 }
 #[cfg(test)]
 mod tests {
@@ -46,7 +55,8 @@ mod tests {
     fn comm_test() {
         let test_msg = "test message\n";
         let mut cell = create_cell("Cell:Test");
-        let msg = talk_to_cell(&mut cell, test_msg);
+        talk_to_cell(&mut cell, test_msg);
+        let msg = &listen_to_cells(&mut vec![&mut cell])[0];
         assert_eq!(test_msg.trim(), msg);
     }
 }
